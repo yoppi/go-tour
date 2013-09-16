920107eb04375113d3aa269bd0e1e000400b5ce9
yoppi <y.hirokazu@gmail.com>
2013-09-16 19:54:45 +0900

    28章 構造体の初期化方法
    フィールドの一部分だけ初期化できる

---
+++ sec28.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+        X, Y int
+}
+
+var (
+        p = Vertex{1, 2}
+        q = &Vertex{1, 2}
+        r = Vertex{X:1}
+        s = Vertex{}
+)
+
+func main() {
+        fmt.Println(p, q, r, s)
+}
