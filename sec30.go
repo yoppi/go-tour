62696fe2a90371388e59c8692024e25cdd712bbb
yoppi <y.hirokazu@gmail.com>
2013-09-16 20:05:13 +0900

    30章 sliceは値の配列を参照して長さも保持する
    長さはlen()でアクセスできる
    []Tが型Tのsliceになる

---
+++ sec30.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+        p := []int{2, 3, 5, 7, 11, 13}
+        q := []string{"hoge", "foo"}
+        fmt.Println("p == ", p)
+        fmt.Println("q == ", q, len(q))
+}
