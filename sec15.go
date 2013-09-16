e6a2054dbc7466b02f0881643f99af9405d41e1c
yoppi <y.hirokazu@gmail.com>
2013-09-16 18:43:28 +0900

    15章 定数はconstキーワードを使う。慣習的に先頭は大文字にするのが良さそう

---
+++ sec15.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+const Pi = 3.14
+
+func main() {
+        const World = "世界"
+        fmt.Println("hello", World)
+        const Truth = true
+        fmt.Println("Go rules?", Truth)
+}
